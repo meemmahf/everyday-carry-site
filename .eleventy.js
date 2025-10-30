ee2be268ea2aeb966b622dddc2d0b74a2bcd7c68
Mahfuza Meem <meemmah@msu.edu>
2025-10-29 20:54:27 -0400

    Fix CSS path for Netlify

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
-
-// The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "_site"
+    }
+  };
 };
